28670c1bb4e6e73e651a9f60c5a6da19cef2cb46
congnguyendinh0 <44774137+congnguyendinh0@users.noreply.github.com>
2019-10-31 17:20:39 +0700

    server.js

--- server.js
+++ server.js
@@ -18,8 +18,13 @@ app.use(
 app.use(
   cors({
     preflightContinue: false,
-    origin: '*',
-    allowedHeaders: ["x-auth-token", "content-type","Access-Control-Allow-Headers", 'headers']
+    origin: "*",
+    allowedHeaders: [
+      "x-auth-token",
+      "content-type",
+      "Access-Control-Allow-Headers",
+      "headers"
+    ]
   })
 );
 
@@ -29,7 +34,7 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "./client/sbuild")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
