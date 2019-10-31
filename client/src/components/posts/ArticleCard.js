cee27e5aa26e2fae4ba158c524e40532c848cc74
congnguyendinh0 <44774137+congnguyendinh0@users.noreply.github.com>
2019-10-31 16:38:52 +0700

    sdasd

--- client/src/components/posts/ArticleCard.js
+++ client/src/components/posts/ArticleCard.js
@@ -1,83 +1,93 @@
-import { Card, CardHeader, Avatar, IconButton, CardContent, CardActions, Button, Badge } from "@material-ui/core";
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import { Link } from 'react-router-dom'
-import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
-
+import {
+  Card,
+  CardHeader,
+  Avatar,
+  IconButton,
+  CardContent,
+  CardActions,
+  Button,
+  Badge
+} from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function createMarkup(post) {
   return {
     __html: `${post.text}`
-  }
+  };
 }
 
 export default function ArticleCard(props) {
-  const { post, user } = props // post.likes and user._id
-  const [liked, setLiked] = useState(false)
-  const [likeCount, setLikeCount] = useState(null)
+  const { post, user } = props; // post.likes and user._id
+  const [liked, setLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(null);
   useEffect(() => {
     if (post) {
-      setLikeCount(post.likes.length)
-
+      setLikeCount(post.likes.length);
     }
-  }, [])
+  }, []);
   useEffect(() => {
-    checkAlreadyLiked()
-  }, [liked, user])
+    checkAlreadyLiked();
+  }, [liked, user]);
 
   const checkAlreadyLiked = async () => {
     if (user && post) {
-      let alreadyLikes = post.likes.forEach((obj) => {
-
+      let alreadyLikes = post.likes.forEach(obj => {
         if (obj.user === user._id) {
-          setLiked(true)
+          setLiked(true);
         }
-      })
-      console.log(alreadyLikes)
+      });
+      console.log(alreadyLikes);
     }
-  }
+  };
 
   const like = async () => {
-    console.log('like')
+    console.log("like");
 
     await Axios({
-      method: 'PUT',
+      method: "PUT",
       url: `http://localhost:5000/api/posts/like/${post._id}`,
       headers: {
-        'x-auth-token': `${localStorage.getItem('token')}`
+        "x-auth-token": `${localStorage.getItem("token")}`
       }
-    }).then(res => {
-      setLiked(true)
-      setLikeCount(likeCount + 1)
-      console.log(res)
-    }).catch(err => {
-      console.log(err)
     })
-  //   setTimeout(() => {
-  //     window.location.reload()
-  //   }, 2000)
-  }
+      .then(res => {
+        setLiked(true);
+        setLikeCount(likeCount + 1);
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    //   setTimeout(() => {
+    //     window.location.reload()
+    //   }, 2000)
+  };
 
   const dislike = async () => {
-    console.log('dislike')
+    console.log("dislike");
 
     await Axios({
-      method: 'PUT',
+      method: "PUT",
       url: `http://localhost:5000/api/posts/unlike/${post._id}`,
       headers: {
-        'x-auth-token': `${localStorage.getItem('token')}`
+        "x-auth-token": `${localStorage.getItem("token")}`
       }
-    }).then(res => {
-      setLiked(false)
-      setLikeCount(likeCount - 1)
-      console.log(res)
-    }).catch(err => {
-      console.log(err)
     })
+      .then(res => {
+        setLiked(false);
+        setLikeCount(likeCount - 1);
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
     // setTimeout(() => {
     //   window.location.reload()
     // }, 1000)
-  }
+  };
 
   const showLike = liked ? (
     <div>
@@ -88,46 +98,34 @@ export default function ArticleCard(props) {
       </Badge>
     </div>
   ) : (
-      <div>
-        <Badge badgeContent={likeCount} color="primary">
-          <IconButton onClick={like}>
-            <FavoriteIcon color="primary"></FavoriteIcon>
-          </IconButton>
-        </Badge>
-      </div>
-    )
+    <div>
+      <Badge badgeContent={likeCount} color="primary">
+        <IconButton onClick={like}>
+          <FavoriteIcon color="primary"></FavoriteIcon>
+        </IconButton>
+      </Badge>
+    </div>
+  );
   return (
     <div>
       <Card>
         <CardHeader
-          avatar={
-            <Avatar
-              src={post.user.avatar}
-
-            />
-          }
-          action={
-            showLike
-          }
+          avatar={<Avatar src={post.user.avatar} />}
+          action={showLike}
           title={post.title}
           subheader={
-            <Link to={`/profile/${post.user._id}`}>
-              {post.user.name}
-            </Link>
+            <Link to={`/profile/${post.user._id}`}>{post.user.name}</Link>
           }
-
         />
         <CardContent>
           <div dangerouslySetInnerHTML={createMarkup(post)}></div>
         </CardContent>
         <CardActions>
           <Link to={`/posts/${post._id}`}>
-            <Button>
-              Read more!
-                  </Button>
+            <Button>Read more!</Button>
           </Link>
         </CardActions>
       </Card>
     </div>
-  )
+  );
 }
--- client/src/components/posts/Comment.js
+++ client/src/components/posts/Comment.js
@@ -1,68 +1,66 @@
-import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
-import { Link } from 'react-router-dom'
-import { Card, CardHeader, Avatar, CardContent, IconButton } from '@material-ui/core'
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
+import {
+  Card,
+  CardHeader,
+  Avatar,
+  CardContent,
+  IconButton
+} from "@material-ui/core";
 
-import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteIcon from "@material-ui/icons/Delete";
 export default function Comment(props) {
-    const { comment, post, fetchPost, user } = props
-    useEffect(() => {
+  const { comment, post, fetchPost, user } = props;
+  useEffect(() => {}, []);
 
-    }, [])
+  const deleted = async comment => {
+    await Axios({
+      method: "DELETE",
+      url: `http://localhost:5000/api/posts/comment/${post._id}/${comment}`,
+      headers: {
+        "x-auth-token": `${localStorage.getItem("token")}`
+      }
+    })
+      .then(res => {
+        console.log(res);
+        fetchPost();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-    const deleted = async (comment) => {
-        await Axios({
-            method: "DELETE",
-            url: `http://localhost:5000/api/posts/comment/${post._id}/${comment}`,
-            headers: {
-                'x-auth-token': `${localStorage.getItem('token')}`
-            }
-        }).then(res => {
-            console.log(res)
-            fetchPost();
-        }).catch(err => {
-            console.log(err)
-        })
+  function checkUser(comment, user) {
+    if (!user && !comment) {
+      return "";
     }
-
-    function checkUser(comment, user) {
-        if (!user && !comment) {
-            return ''
-        }
-        if (user._id == comment.user) {
-            return (
-                <IconButton onClick={() => deleted(comment._id)}>
-                    <DeleteIcon></DeleteIcon>
-                </IconButton>
-            )
-        } else {
-            return ''
-        }
+    if (user._id == comment.user._id) {
+      return (
+        <IconButton onClick={() => deleted(comment._id)}>
+          <DeleteIcon></DeleteIcon>
+        </IconButton>
+      );
+    } else {
+      return "";
     }
+  }
 
-    const showDelete = checkUser(comment, user)
+  const showDelete = checkUser(comment, user);
 
-    return (
-        <Card>
-            <CardHeader
-                avatar={
-                    <Avatar
-                        src={comment.user.avatar}
-                    />
-                }
-                action={
-                    showDelete
-                }
-                title={comment.text}
-                subheader={
-                    <Link to={`/profile/${comment.user._id}`}>
-                        {comment.user.name}
-                    </Link>
-                }
-            />
-            <CardContent>
-                <p>{comment.text}</p>
-            </CardContent>
-        </Card>
-    )
+  return (
+    <Card>
+      <CardHeader
+        avatar={<Avatar src={comment.user.avatar} />}
+        action={showDelete}
+        title={comment.text}
+        subheader={
+          <Link to={`/profile/${comment.user._id}`}>{comment.user.name}</Link>
+        }
+      />
+      <CardContent>
+        <p>{comment.text}</p>
+      </CardContent>
+    </Card>
+  );
 }
